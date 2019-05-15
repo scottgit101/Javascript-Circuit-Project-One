d2bb01476a2ca50fe5fcf9ce54026bc80c24d799
scottgit101 <45050002+scottgit101@users.noreply.github.com>
2019-05-14 22:17:17 -0500

    Add files via upload

---
+++ psuedocode.js
@@ -0,0 +1,54 @@
+//Contact form for users to deliver message to site adminstrators. 
+
+
+
+//First, code html "contact" form. Include input fields "name", "email", & textarea "message" for data entry. Include submit button for users to submit their data.
+
+
+
+//For input fields "name", "email", & textarea "message":
+
+
+
+//Name: Undefined variable where users input their name as a string value.
+
+//If onlick "submit" field has value, then assign value to "name". Submission successful. 
+
+//Else return error message "Please enter'name'".
+
+
+
+//Email: Undefined variable where users input their name as a string value.
+
+//If onlick "submit" field has value, then assign value to "email". Submission successful. 
+
+//Else return error message "Please enter'email'".
+
+
+
+//Message: Undefined variable where users input their name as a string value.
+
+//If onlick "submit" field has value, then assign value to "message". Submission successful. 
+
+//Else return error message "Please enter'message'".
+
+
+
+//For submit button:
+
+//Create event listener for cursor click on submit button.
+
+//If onclick "submit" input fields & textarea have values return "Thank you" message. Submission successful. 
+
+//Else return error message in incomplete field.
+
+//On successful submission store user values in array.
+
+
+
+
+
+
+
+
+
